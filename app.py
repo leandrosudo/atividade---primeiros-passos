5ff75fd2dff59baa2aa35e648e0da06fac174362
Leandro <leandro.lafurtado@gmail.com>
2026-05-24 13:10:04 -0300

    Adiciona rotas no Flask '/' e 'Contato'

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def exibir_home():
+    return render_template("index.html")
+
+@app.route("/contato")
+def exibir_contato():
+    return render_template("contato.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
